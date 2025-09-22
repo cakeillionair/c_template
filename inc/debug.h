a2d2d5b30bc26a3e495137b29611782b30e1107a
cakeillionair <jan.breithaupt@gmx.net>
2025-09-22 19:08:29 +0200

    0.1.3 - fixed dbg_printf - added dbg_assert

--- inc/debug.h
+++ inc/debug.h
@@ -3,10 +3,12 @@
 
 #ifdef DEBUG
 # define dbg_code(...) __VA_ARGS__
-# define dbg_printf(fmt, ...) printf("[DBG:%16s:%3.3d]"fmt, __FILE__, __LINE__, __VA_OPT__(,) __VA_ARGS__)
+# define dbg_printf(fmt, ...) printf("[DBG:%16s:%3.3d]"fmt, __FILE__, __LINE__ __VA_OPT__(,) __VA_ARGS__)
+# define dbg_assert(b) if (!(b)) dbg_printf("ASSERT FAILED! (%s)\n", #s), exit(1)
 #else
 # define dbg_printf(fmt, ...) ((void) 0)
 # define dbg_code(...)
+# define dbg_assert(b) ((void) 0)
 #endif
 
 #endif
